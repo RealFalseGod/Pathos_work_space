b52f40f948040e2f1bb684f15339df0856b82405
RealFalseGod <chouhanbhagwan1@gmail.com>
2025-03-02 13:00:05 +0530

    tailwind css setup done too ahahaa

---
+++ app/(roots)/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    <Text className="font-bold text-lg my-10">Welcome to my App</Text>
+    <Link href='/sign_in'> Sign in</Link>
+    </View>
+  );
+}
