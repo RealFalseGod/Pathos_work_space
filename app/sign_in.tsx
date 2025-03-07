d284532583c05024ec6bae3046fcc54605b23b2e
RealFalseGod <chouhanbhagwan1@gmail.com>
2025-03-08 00:17:10 +0530

    splash screen and logo

--- app/sign_in.tsx
+++ app/sign_in.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const sign_in = () => {
   return (
-    <View>
-      <Text>sign in</Text>
+    <View className='bg-white h-full flex items-center'>
+    <Image source={require('../assets/images/logo.png')} resizeMode='contain' className='w-1/2'/>
+    <Text className="-mt-50 text-lg font-semibold text-center">
+    Level up and improve
+  </Text>
+   
+      
     </View>
   )
 }
